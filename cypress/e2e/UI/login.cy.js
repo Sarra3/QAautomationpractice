b11d943dd54e64950162881affbaf30a1ccc4af3
Sarra3 <sarra.b30@gmail.com>
2022-12-14 21:49:08 +0100

    qa cypress practice

--- cypress/e2e/UI/login.cy.js
+++ cypress/e2e/UI/login.cy.js
@@ -2,13 +2,17 @@ import homePage from "../../pages/homepage"
 import loginPage from "../../pages/loginpage"
 import searchPage from "../../pages/search"
 import advancedPage from "../../pages/advence"
-import validationPage from "../../pages/validation"
-import menusearchPage from "../../pages/menusearch"
+import menuSearchPage from "../../pages/menusearch"
+import registerForm from "../../pages/registerForm"
+
 
 describe('Login tests', () => {
   beforeEach(() => {
     homePage.navigateTo()
-    searchPage.searchItem('Apple MacBook Pro 13-inch')
+  })
+
+  it('register', () =>{
+    registerForm.registerUser()
   })
 
   it('Validate login', () => {
@@ -16,26 +20,39 @@ describe('Login tests', () => {
     loginPage.loginTo('sa@gmail.com', '12341234')
     homePage.validateLogoutIsVisible()
   })
+})
+
+describe('search', () =>{
+  beforeEach(() =>{
+    searchPage.searchItem('Apple MacBook Pro 13-inch')
+  })
 
   it('Search',  () =>{
     searchPage.selectItem()
   });
 
+describe('Advanced search positive value', () =>{
   it('advance search positive value',  () => {
     advancedPage.advancedSearch('3')
-    validationPage.positiveValidation()
+    advancedPage.positiveValidation()
   });
+})
 
+describe('Advanced search negative value', () =>{
   it('advance search negative value ', ()  => {
     advancedPage.advancedSearch('5')
-    validationPage.notFoundMessageValidation()
+    advancedPage.notFoundMessageValidation()
   });
+})
+})
 
+describe('dropdown menu search', () =>{
   it('dropdown menu search ', () => {
-    menusearchPage.menuSearch()
-
+    menuSearchPage.menuSearch()
   });
+})
 
+describe('invalid login', () =>{
   it('Invalid login', () => {
     homePage.clickOnLogin()
     loginPage.loginTo('sarr@gmail.com', '123123')
--- cypress/pages/advence.js
+++ cypress/pages/advence.js
@@ -1,17 +1,28 @@
-class AdvancedSearch{
-    advancedSearch(value){
+class AdvancedSearch {
+    advancedSearch(value) {
         this.elements.advanced().click()
         this.elements.category().select(value)
         this.elements.search().click()
 
     }
 
+    positiveValidation() {
+        this.elements.product().should('be.visible')
+    }
+
+    notFoundMessageValidation() {
+        this.elements.message().should('be.visible')
+    }
+
     elements = {
         advanced: () => cy.get('#advs'),
         category: () => cy.get('#cid'),
         search: () => cy.get('.search-button'),
-
+        product: () => cy.get('.product-item'),
+        message: () => cy.get('.no-result')
     }
+
 }
+
 
 module.exports = new AdvancedSearch()
